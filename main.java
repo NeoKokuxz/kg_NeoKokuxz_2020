c508570793bdd0e3faa3e02f38c6a238337f15de
NeoKokuxz <neko.kokubyakuin@yahoo.com>
2020-03-14 02:18:13 -0400

    Complete Implementation with different test cases

--- main.java
+++ main.java
@@ -1,57 +1,63 @@
 import java.util.HashMap;
-import java.util.Map;
-
 public class main {
     public static void main(String[] args) {
-        String s1 = "1231";
-        String s2 = "321";
+        String s1 = "abc";
+        String s2 = "bcd";
         System.out.println("Test Case 1: abc vs bcd");
-        isOneToOne(s1,s2);
-//        String s3 = "foo";
-//        String s4 = "bar";
-//        System.out.println("Test Case 2: foo vs bar");
-//        System.out.println(isOneToOne(s3,s4));
-
+        System.out.println( isOneToOne(s1,s2) );
+        String s3 = "foo";
+        String s4 = "bar";
+        System.out.println("Test Case 2: foo vs bar");
+        System.out.println( isOneToOne(s3,s4) );
+        String s5 = "bar";
+        String s6 = "foo";
+        System.out.println("Test Case 3: bar vs foo");
+        System.out.println( isOneToOne(s5,s6) );
+        String s7 = "barrr";
+        String s8 = "foo";
+        System.out.println("Test Case 4: barrr vs foo");
+        System.out.println( isOneToOne(s7,s8) );
     }
 
-    public static void isOneToOne(String s1, String s2) {
-
-        //Create 2 HashMap for each character in String 1 and String 2
-        HashMap<String, Integer> S1Map = new HashMap<>();
-        HashMap<String, Integer> S2Map = new HashMap<>();
+    public static boolean isOneToOne(String s1, String s2) {
 
         //Read each Character from both String and put into Char Array
         char[] S1Array = s1.toCharArray();
         char[] S2Array = s2.toCharArray();
 
-        //Loop through Char Array and put into Hash Map
-        for(char x : S1Array ){
-            //Convert X int String
-            String currentX = Character.toString(x);
-            System.out.println(currentX);
-            if(!S1Map.containsKey(currentX)){
-                S1Map.put(currentX, 1);
-            } else {
-                System.out.println("Duplicate Found");
+        //HashMap for checking duplicate keys
+        HashMap<String, String> HM = new HashMap<>();
+
+        //if length match - continue
+        if(lengthCheck(s1, s2)){
+            //Read from Char Array into HashMap
+            for(int i = 0; i < s1.length(); i++){
+                //Turn both Char Array into String type
+                String currentX = String.valueOf(S1Array[i]);
+                String currentY = String.valueOf(S2Array[i]);
+                //If key seen before, stop and return false
+                if(!HM.containsKey(currentX)){
+                    HM.put( currentX, currentY);
+                } else {
+                    return false;
+                }
             }
+            //Print Success Mapping
+            System.out.println(HM);
+            return true;
+        } else {
+            //Length not matching
+            return false;
         }
 
-        System.out.println(S1Map);
+    }
 
-//        //Loop through Char Array and put into Hash Map
-//        for(char y : S2Array) {
-//            String currentY = Character.toString(y);
-//            for (Map.Entry<String, Integer> entry : S2Map.entrySet()) {
-//                S2Map.put(currentY, 1);
-//                System.out.println( "Current Key " + entry.getKey() );
-//            }
-//        }
-
-//        if(S1Map.size() != S2Map.size()){
-//            return false;
-//        } else {
-//            //TODO: Implement Condition check for 1to1 between 2 HashMap
-//            return true;
-//        }
+    public static boolean lengthCheck(String s1, String s2){
+        if(s1.length() != s2.length()){
+            System.out.println("Length doesn't match");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
